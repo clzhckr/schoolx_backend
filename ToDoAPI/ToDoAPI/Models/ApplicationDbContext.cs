fdc7c9e5011a26126c8f428a24ee20ae0155f3c1
clzhckr <57432105+clzhckr@users.noreply.github.com>
2022-05-03 19:06:36 +0300

    drop

---
+++ ToDoAPI/ToDoAPI/Models/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Authentication;
+
+namespace ToDoAPI.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ToDoItemModel> ToDoItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ToDoItemModel>(entity =>
+            {
+                entity.Property(e => e.ItemName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+                entity.Property(e => e.ItemDescription)
+                .IsRequired()
+                .HasMaxLength(100);
+
+                entity.Property(e => e.ItemStatus)
+                .IsRequired()
+                .HasMaxLength(1);
+
+                /*entity.Property(e => e.DateTime)
+                .IsRequired();
+                .HasMaxLength(30); */
+
+            });
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
